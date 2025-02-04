fb7765a561b9d439d619509062d2acf2c5ea99a4
CarlosAndreo <c.andreo@uma.es>
2025-02-04 16:16:22 +0000

    add: lucide react for icons

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,40 +1,52 @@
+import { FilePlus, AlertCircle, Lightbulb } from 'lucide-react';
 import styles from './styles.module.css';
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          <div className="col col--4">
-            <div className="text--center">
+      <div className='container'>
+        <div className='row'>
+          <div className='col col--4'>
+            <div className='text--center'>
               <h3>Contributing</h3>
+              <FilePlus size={30} className={styles.icon} />
               <p>
                 Want to help improve the documentation? <br />
-                <a href="https://6g-sandbox.github.io/docs/contributing" target="_blank" rel="noopener noreferrer">
+                <a href='https://6g-sandbox.github.io/docs/contributing' target='_blank' rel='noopener noreferrer'>
                   Contribute here
                 </a>
               </p>
             </div>
           </div>
 
-          <div className="col col--4">
-            <div className="text--center">
+          <div className='col col--4'>
+            <div className='text--center'>
               <h3>Report an Issue</h3>
+              <AlertCircle size={30} className={styles.icon} />
               <p>
                 Found a problem with the documentation? <br />
-                <a href="https://github.com/6G-SANDBOX/docs/issues/new?assignees=&labels=&projects=&template=bug_report.md" target="_blank" rel="noopener noreferrer">
+                <a
+                  href='https://github.com/6G-SANDBOX/docs/issues/new?assignees=&labels=&projects=&template=bug_report.md'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   Report it here
                 </a>
               </p>
             </div>
           </div>
 
-          <div className="col col--4">
-            <div className="text--center">
+          <div className='col col--4'>
+            <div className='text--center'>
               <h3>Suggest an Improvement</h3>
+              <Lightbulb size={30} className={styles.icon} />
               <p>
                 Got an idea to improve the documentation? <br />
-                <a href="https://github.com/6G-SANDBOX/docs/issues/new?assignees=&labels=&projects=&template=feature_request.md" target="_blank" rel="noopener noreferrer">
+                <a
+                  href='https://github.com/6G-SANDBOX/docs/issues/new?assignees=&labels=&projects=&template=feature_request.md'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   Suggest here
                 </a>
               </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,26 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
+import { Construction, Rocket } from 'lucide-react';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
+      <div className='container'>
+        <Heading as='h1' className='hero__title'>
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <p className="hero__description">
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
+        <p className='hero__description'>
           This documentation provides comprehensive guides, tutorials and resources for using the tools and technologies of the 6G-SANDBOX project.
         </p>
-        <p className="hero__work-in-progress">
-          🚧 We are actively working on improving and expanding this documentation. Stay tuned for updates! 🚀
+        <p className='hero__work-in-progress'>
+          <Construction size={20} className={styles.icon} /> We are actively working on improving and expanding this documentation. Stay tuned for updates! <Rocket size={20} className={styles.icon} />
         </p>
         <div className={styles.buttons}>
-          <a className="button button--secondary button--lg" href="/docs/introduction">
+          <a className='button button--secondary button--lg' href='/docs/introduction'>
             Get Started
           </a>
         </div>
@@ -36,7 +37,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title} DOCS`}
-      description="Description will go into a meta tag in <head />">
+      description='Description will go into a meta tag in <head />'>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
