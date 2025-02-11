900c6b1d1197053510ddd15c480790c255592f31
CarlosAndreo <c.andreo@uma.es>
2025-02-11 07:50:34 +0000

    fix: p comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <p className='hero__description'>
-          This documentation provides comprehensive guides, tutorials and resources for using the tools and technologies of the 6G-SANDBOX project.
+          This documentation provides comprehensive guides, tutorials and resources for using the tools developed in 6G-SANDBOX project.
         </p>
         <p className='hero__work-in-progress'>
           <Construction size={20} className={styles.icon} /> We are actively working on improving and expanding this documentation. Stay tuned for updates! <Rocket size={20} className={styles.icon} />
