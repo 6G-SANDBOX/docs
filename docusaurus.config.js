2279e46f4e5c04d054a782f877c6a2e0ca1a1926
CarlosAndreo <c.andreo@uma.es>
2025-03-13 16:35:35 +0000

    add: marketplace icon to redirect to marketplace

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,6 +121,21 @@ const config = {
             className: 'header-github-link',
             'aria-label': 'GitHub repository'
           },
+          {
+            href: 'https://marketplace.mobilesandbox.cloud:9443/appliance',
+            position: 'right',
+            className: 'header-marketplace-link',
+            'aria-label': 'Marketplace',
+            html: `
+              <div style="display: flex; align-items: center; justify-content: center; height: 100%;">
+                <svg class="marketplace-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <circle cx="8" cy="21" r="1"/>
+                  <circle cx="19" cy="21" r="1"/>
+                  <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/>
+                </svg>
+              </div>
+            `,
+          },
         ],
         hideOnScroll: true,
       },
@@ -187,7 +202,6 @@ const config = {
         backgroundColor: '#fffae6',
         textColor: '#091E42',
         isCloseable: true,
-
       },
     }),
 };
